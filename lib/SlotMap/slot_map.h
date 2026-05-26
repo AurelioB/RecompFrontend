14154526172f5a056e663288951665bf41617742
Aurelio Bernal <aurelio@bernalr.com>
2026-05-26 01:01:49 -0600

    fix: use posix_memalign for SlotMap

--- lib/SlotMap/slot_map.h
+++ lib/SlotMap/slot_map.h
@@ -48,7 +48,14 @@
 #else
 // Posix
 #include <stdlib.h>
-#define SLOT_MAP_ALLOC(sizeInBytes, alignment) aligned_alloc(alignment, sizeInBytes)
+static inline void* slot_map_posix_alloc(size_t sizeInBytes, size_t alignment) {
+    void* ptr = nullptr;
+    if (posix_memalign(&ptr, alignment, sizeInBytes) != 0) {
+        return nullptr;
+    }
+    return ptr;
+}
+#define SLOT_MAP_ALLOC(sizeInBytes, alignment) slot_map_posix_alloc(sizeInBytes, alignment)
 #define SLOT_MAP_FREE(ptr) free(ptr)
 #endif
 
